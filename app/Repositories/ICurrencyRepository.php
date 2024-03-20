a0a62a0bcd4b057ed2896ed5470fad04611e13ea
Lucas Yamaguchi <luccahideki@hotmail.com>
2024-03-19 21:59:17 -0300

    feat: inclusão de endpoint para consulta de dados

--- app/Repositories/ICurrencyRepository.php
+++ app/Repositories/ICurrencyRepository.php
@@ -4,7 +4,8 @@ namespace App\Repositories;
 
 interface ICurrencyRepository
 {
-    public function create(array $data);
-    public function update(array $oldData, array $newData) ;
-    public function findByCode(string $code);
+    public function create(array $data): void;
+    public function update(array $oldData, array $newData): void;
+    public function findByCode(string $code): ?array;
+    public function find(array $filters): ?array;
 }
--- app/Services/ICurrencyService.php
+++ app/Services/ICurrencyService.php
@@ -5,4 +5,5 @@ namespace App\Services;
 interface ICurrencyService
 {
     public function createOrUpdate(array $data): void;
+    public function find(array $filters): ?array;
 }
